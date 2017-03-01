a478246e581a2aba384d4740488f1c1d2abb7316
GoFo <gfm10001@student.mdh.se>
2017-03-01 18:21:35 +0100

    adjusted...

---
+++ main.c
@@ -0,0 +1,21 @@
+// main.c
+// ARC GIT-Lab 2017-03-01
+
+//
+// GIT commands:
+// =============
+// 
+// git clone https://github.com/gfm10001/arc-git-intro.git
+//
+// git status
+// git add .
+
+
+#include <stdio.h>
+
+int main(void)
+{
+   printf("\nHej Git!\n\n");
+   return 0;
+   
+} // main
